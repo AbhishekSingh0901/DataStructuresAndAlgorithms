a1b0b4840c33756cfb21535880c1ee9a7fb67ee8
Abhishek Singh <as9284608@gmail.com>
2022-09-01 13:15:16 +0530

    more into big O

---
+++ BigONotiation/analysingPerformanceOfArraysAndObjects.js
@@ -0,0 +1,14 @@
+//obejets- unordered DS
+
+let me = {
+  firstName: "Abhi",
+  lastName: "Singh",
+  age: 22,
+  hobbies: ["Cricket", "Coding", "Dancing", "Music", "Sketching"],
+};
+
+console.log(Object.keys(me));
+console.log(Object.entries(me));
+console.log(me.hasOwnProperty("firstName"));
+
+//this section is just about the time and space complexites of various array and object method
