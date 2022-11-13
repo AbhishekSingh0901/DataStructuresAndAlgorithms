db739007adb3ebc694ce1d720345549e039f88aa
Abhishek Singh <as9284608@gmail.com>
2022-11-13 14:21:15 +0530

    quick sort alogo

--- BetterSortingAlgorithms/quickSort.js
+++ BetterSortingAlgorithms/quickSort.js
@@ -1,17 +1,27 @@
 //Pivot helper
 
-function pivotHelper(arr, start = 0, end = arr.length + 1) {
+function pivotHelper(arr, start = 0, end = arr.length - 1) {
   let pivot = arr[start];
-  let swapIndex = 0;
+  let swapIndex = start;
   for (let i = start + 1; i < arr.length; i++) {
     if (arr[i] < pivot) {
       swapIndex++;
       [arr[swapIndex], arr[i]] = [arr[i], arr[swapIndex]];
-      console.log(arr);
+      // console.log(arr);
     }
   }
   [arr[start], arr[swapIndex]] = [arr[swapIndex], arr[start]];
+  // console.log(arr);
   return swapIndex;
 }
-
-pivotHelper([4, 8, 2, 1, 5, 7, 6, 3]);
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivotHelper(arr, left, right);
+    //left
+    quickSort(arr, left, pivotIndex - 1);
+    //right
+    quickSort(arr, pivotIndex + 1, right);
+  }
+  return arr;
+}
+console.log(quickSort([100, -3, 2, 4, 6, 9, 1, 2, 5, 3, 23]));
