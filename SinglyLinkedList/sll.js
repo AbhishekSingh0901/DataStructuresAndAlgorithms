49e4ec9aa4d740cfaf0175fc9de18706f48a1e32
Abhishek Singh <as9284608@gmail.com>
2022-12-04 10:38:36 +0530

    creating node, SLL adding push method

---
+++ SinglyLinkedList/sll.js
@@ -0,0 +1,39 @@
+//peice of data - val
+//reference to next node - next
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  push(val) {
+    let newNode = new Node(val);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+    return this;
+  }
+}
+// var first = new Node("Hi");
+// first.next = new Node("There");
+// first.next.next = new Node("how");
+// first.next.next.next = new Node("are");
+// first.next.next.next.next = new Node("you");
+
+// console.log(first);
+let list = new SinglyLinkedList();
+console.log(list);
+console.log(list.push("hi"));
+console.log(list.push("hello!"));
