7b68eb2907798edc1f60a78d693c1474646862e4
Abhishek Singh <as9284608@gmail.com>
2023-01-28 13:14:16 +0530

    completing SLL

--- SinglyLinkedList/sll.js
+++ SinglyLinkedList/sll.js
@@ -70,6 +70,80 @@ class SinglyLinkedList {
     this.length++;
     return this;
   }
+
+  get(index) {
+    if (index < 0 || index >= this.length) {
+      return null;
+    } else {
+      let valNode = this.head;
+      for (let i = 0; i < index; i++) {
+        valNode = valNode.next;
+      }
+      return valNode;
+    }
+  }
+
+  set(value, index) {
+    const curVal = this.get(index);
+    if (curVal) {
+      curVal.val = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(val, index) {
+    if (index < 0 || index > this.length) return false;
+    else if (index === 0) this.unshift(val);
+    else if (index === this.length) this.push(val);
+    else {
+      let newNode = new Node(val);
+      let prevNode = this.get(index - 1);
+      newNode.next = prevNode.next;
+      prevNode.next = newNode;
+      this.length++;
+    }
+    return true;
+  }
+
+  remove(index) {
+    if (index < 0 || index >= this.length) return undefined;
+    else if (index === 0) return this.shift();
+    else if (index === this.length - 1) return this.pop();
+    else {
+      let prevNode = this.get(index - 1);
+      let removed = prevNode.next;
+      prevNode.next = removed.next;
+      this.length--;
+      return removed;
+    }
+  }
+
+  print() {
+    let arr = [];
+    let current = this.head;
+    while (current) {
+      arr.push(current.val);
+      current = current.next;
+    }
+    console.log(arr);
+  }
+
+  reverse() {
+    let node = this.head;
+    this.head = this.tail;
+    this.tail = node;
+    let next;
+    let prev = null;
+
+    for (let i = 0; i < this.length; i++) {
+      next = node.next;
+      node.next = prev;
+      prev = node;
+      node = next;
+    }
+    return this;
+  }
 }
 // var first = new Node("Hi");
 // first.next = new Node("There");
@@ -79,12 +153,24 @@ class SinglyLinkedList {
 
 // console.log(first);
 let list = new SinglyLinkedList();
-list.push("hi,");
-list.push("hello");
-list.push("Brothoer!");
-list.push("Welcome");
-list.push("here");
-console.log(list);
-list.shift();
-list.unshift("Yo");
-list.traverse();
+// list.push("hi,");
+// list.push("hello");
+// list.push("Brothoer!");
+// list.push("Welcome");
+// list.push("here");
+// console.log(list);
+// list.shift();
+// list.unshift("Yo");
+// list.traverse();
+// console.log("--------------------");
+// console.log(list.get(3));
+list.push(100);
+list.push(200);
+list.push(300);
+list.push(400);
+list.push(500);
+list.push(600);
+list.push(700);
+list.print();
+list.reverse();
+list.print();
