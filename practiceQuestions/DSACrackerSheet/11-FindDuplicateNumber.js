b7319857d1afd8d3de7b5914411208de2a452716
Abhishek Singh <as9284608@gmail.com>
2023-01-31 12:09:15 +0530

    Floyd's cycle

--- practiceQuestions/DSACrackerSheet/11-FindDuplicateNumber.js
+++ practiceQuestions/DSACrackerSheet/11-FindDuplicateNumber.js
@@ -1,3 +1,4 @@
+//using Nlog(N) TC:
 const findDuplicate = function (nums) {
   let n = nums.length - 1;
   //using sort:
@@ -14,3 +15,27 @@ const findDuplicate = function (nums) {
 // console.log(findDuplicate([1, 3, 4, 2, 2]));
 // console.log(findDuplicate([1, 3, 4, 2, 2]));
 console.log(findDuplicate([3, 1, 3, 4, 2]));
+
+//Optimised Solution: using flyods cycle:
+
+const optimisedFindDuplicate = function (nums) {
+  let fast = 0;
+  let slow = 0;
+
+  while (true) {
+    fast = nums[nums[fast]];
+    slow = nums[slow];
+
+    if (fast === slow) {
+      let pointer = 0;
+      while (pointer !== slow) {
+        pointer = nums[pointer];
+        slow = nums[slow];
+      }
+
+      return pointer;
+    }
+  }
+};
+
+console.log(optimisedFindDuplicate([3, 1, 3, 4, 2]));
