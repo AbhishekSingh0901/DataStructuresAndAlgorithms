4a958957cdac692ab23adcdd18cffc30faac64e2
Abhishek Singh <as9284608@gmail.com>
2023-02-03 12:11:20 +0530

    travesal in tree

--- BinarySearchTree/bst.js
+++ BinarySearchTree/bst.js
@@ -14,18 +14,20 @@ class BinarySearchTree {
 
   insert(val) {
     const node = new Node(val);
-    if (!this.root) this.root = node;
-    else {
+    if (!this.root) {
+      this.root = node;
+      return this;
+    } else {
       let currnode = this.root;
       while (true) {
-        if (val === currnode.val) return undefined;
-        if (val < currnode.right) {
+        if (val === currnode.value) return undefined;
+        if (val < currnode.value) {
           if (currnode.left === null) {
             currnode.left = node;
             return this;
           }
           currnode = currnode.left;
-        } else if (val > currnode.right) {
+        } else {
           if (currnode.right === null) {
             currnode.right = node;
             return this;
@@ -53,4 +55,64 @@ class BinarySearchTree {
     }
     return undefined;
   }
+
+  bfs() {
+    const data = [];
+    const queue = [];
+    let node = this.root;
+
+    queue.push(node);
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.value);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    return data;
+  }
+
+  preOrder() {
+    let data = [];
+    function traverse(node) {
+      data.push(node.value);
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
+
+  postOrder() {
+    let data = [];
+    // let current = this.root
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right);
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+  postOrder() {
+    let data = [];
+    // let current = this.root
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      data.push(node.value);
+      if (node.right) traverse(node.right);
+    }
+    traverse(this.root);
+    return data;
+  }
 }
+
+let myTree = new BinarySearchTree();
+myTree.insert(10);
+myTree.insert(6);
+myTree.insert(15);
+myTree.insert(3);
+myTree.insert(8);
+myTree.insert(20);
+console.log(myTree);
+console.log(myTree.bfs());
+console.log(myTree.postOrder());
