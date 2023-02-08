4afae6ecc6ae8f6967f47c6dbaf35ba6e148b7c3
Abhishek Singh <as9284608@gmail.com>
2023-02-08 13:08:37 +0530

    ExtractMax property of Max Binary Heap

--- BInaryHeap/index.js
+++ BInaryHeap/index.js
@@ -22,6 +22,47 @@ class MaxBinaryHeap {
       idx = parentidx;
     }
   }
+
+  extractMac() {
+    const max = this.values[0];
+    const end = this.values.pop();
+    this.values[0] = end;
+
+    //Bubble down:
+    this.sinkDown();
+    return max;
+  }
+  sinkDown() {
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while (true) {
+      let leftIdx = 2 * idx + 1;
+      let rightIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if (leftIdx < length) {
+        leftChild = this.values[leftIdx];
+        if (leftChild > element) {
+          swap = leftIdx;
+        }
+      }
+      if (rightIdx < length) {
+        rightChild = this.values[rightIdx];
+        if (
+          (swap === null && rightChild > element) ||
+          (swap !== null && rightChild > leftChild)
+        ) {
+          swap = rightIdx;
+        }
+      }
+      if (swap === null) break;
+      this.values[idx] = this.values[swap];
+      this.values[swap] = element;
+      idx = swap;
+    }
+  }
 }
 
 let heap = new MaxBinaryHeap();
@@ -33,4 +74,7 @@ heap.insert(27);
 heap.insert(12);
 heap.insert(55);
 
+console.log(heap.values);
+
+console.log(heap.extractMac());
 console.log(heap.values);
