e8240454732fa55e0c7a7000079213938e44c75f
Abhishek Singh <as9284608@gmail.com>
2023-05-08 09:15:30 +0530

    revising count and say

--- practiceQuestions/DSACrackerSheet/42_countAndSay.js
+++ practiceQuestions/DSACrackerSheet/42_countAndSay.js
@@ -1,23 +1,22 @@
 const countAndSay = (n) => {
-  if (n === 1) return "1";
-  if (n === 2) return "2";
-  let s = "11";
-  for (let i = 3; i <= n; i++) {
-    let t = "";
-    s = s + "&";
-    let count = 1;
-    for (let j = 1; j < s.length; j++) {
+  if (n === 1) return "1"
+  if (n === 2) return "11"
+  let s = "11"
+  for (let i = 3; i < n; i++) {
+    let t = ""
+    let count = 1
+    for (let j = 1; j <= s.length; j++) {
       if (s[j] !== s[j - 1]) {
-        t = t + count;
-        t = t + s[j - 1];
-        count = 1;
-      } else count++;
+        t = t + count
+        t = t + s[j - 1]
+        count = 1
+      } else count++
     }
-    s = t;
+    s = t
   }
 
-  return s;
-};
+  return s
+}
 
-console.log(countAndSay(4));
-console.log(countAndSay(5));
+console.log(countAndSay(4))
+console.log(countAndSay(5))
